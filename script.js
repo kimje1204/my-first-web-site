313e90327688da8b34f69e65c17193c0a8e5589e
쥔니 <138183672+kimje1204@users.noreply.github.com>
2023-07-06 21:26:33 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+console.log(-21);
+console.log(3.14);
+console.log('바보   멍청이@@@');
+
+
+//변수로 바뀐 데이터를 log안에 기재할때는 따옴표없이해야함
+//
+
+let 멍청이="오병일"
+console.log(멍청이)
+멍청이="김지은"
+console.log(멍청이)
+
+const bb='돼지';
+console.log(bb)
+
+console.log(bb);
+const 입력= "똑바로 하자!"
+const result = prompt(입력)
+console.log(result)
+
+const str1=`바보 중간에${bb}넣어주기`
+console.log(str1)   
+
+let rresult=3 
+rresult+=3
+console.log(rresult)
+rresult-=2
+console.log(rresult)
+rresult*=7
+console.log(typeof rresult)
+
+let number;
+console.log(number)
+
+let value="true"
+console.log(value)
+
